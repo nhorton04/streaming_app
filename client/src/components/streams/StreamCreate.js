8c55bddca883d143482868bc777409f6465df5a7
Nick <nhorton04@gmail.com>
2019-01-31 19:48:36 -0800

    reformatting

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
-export default StreamCreate;
+class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
