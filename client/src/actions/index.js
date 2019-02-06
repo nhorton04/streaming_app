9f7f248f9073d460d542a2cdf7de5cbe4ba7e42b
Nick <nhorton04@gmail.com>
2019-02-05 23:57:53 -0800

    set up action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -35,5 +35,18 @@ export const fetchStreams = () => async dispatch => {
 };
 
 export const fetchStream = id => async dispatch => {
-  dispatch({ type: FETCH_STREAM });
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
